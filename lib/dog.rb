f36ad18cb6d254322a61badb8a84bb1643d898c8
RudeChowder <scrane88@gmail.com>
2020-04-18 17:40:06 -0400

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Dog
+    def name=(given_name)
+        @name = given_name
+    end
+    
+    def name
+        @name
+    end
+    def bark
+        puts "woof!"
+    end
+end
